7fe9f75fbacb055f3acd227497fbf98eb3e1ba80
gcho <ghcho20@gmail.com>
2019-10-20 17:05:56 +0100

    add bash script

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+INAME=dnet:idq4p
+INAME_CHECK=$(docker images -q $INAME)
+
+if [ "$INAME_CHECK" == "" ]; then
+    docker build --rm -t $INAME .
+fi
+
+CNAME=idq4p
+if [ "$1" != "" ]; then
+	CNAME=$1
+fi
+
+echo "> Launch a container($CNAME)"
+WORKSPACE=${PWD}/ws
+WORKSPACE_MNT=/root/workspace
+DOCKER=docker
+$DOCKER run --rm --name $CNAME -v "$WORKSPACE:$WORKSPACE_MNT" -w "$WORKSPACE_MNT" -it $INAME
+
+exit
