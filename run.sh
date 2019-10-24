0c86c513b252935d49ba5afa84d3df99e1aa6287
gcho <ghcho20@gmail.com>
2019-10-24 01:14:01 +0100

    minor update for cygwin bash

--- run.sh
+++ run.sh
@@ -15,7 +15,8 @@ fi
 echo "> Launch a container($CNAME)"
 WORKSPACE=${PWD}/ws
 WORKSPACE_MNT=/root/workspace
-DOCKER=docker
+DOCKER=$(which --skip-alias winpty 2> /dev/null)
+DOCKER="$DOCKER docker"
 $DOCKER run --rm --name $CNAME -v "$WORKSPACE:$WORKSPACE_MNT" -w "$WORKSPACE_MNT" -it $INAME
 
 exit
